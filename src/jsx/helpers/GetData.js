997b8d659a1034928aee684bf54be566f95704b2
Teemo Tebest <teemo.tebest@gmail.com>
2022-09-28 10:22:09 +0200

    Fixed data fetching in prod

--- src/jsx/helpers/GetData.js
+++ src/jsx/helpers/GetData.js
@@ -1,6 +1,6 @@
 import CSVtoJSON from './CSVtoJSON.js';
 
-export const getData = () => fetch((window.location.href.includes('unctad.org')) ? 'https://storage.unctad.org/2022-sustainable_fund/assets/data/sustainability_fund - data.csv' : './assets/data/sustainability_fund - data.csv')
+export const getData = () => fetch((window.location.href.includes('gsfo.org')) ? 'https://storage.unctad.org/2022-sustainable_fund/assets/data/sustainability_fund - data.csv' : './assets/data/sustainability_fund - data.csv')
   .then(response => response.text())
   .then(body => CSVtoJSON(body));
 
