79a8e97cccd1a2e3c89470655e55f2b56b3cad90
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-05 18:54:43 +0200

    Added command

--- src/jsx/helpers/GetData.js
+++ src/jsx/helpers/GetData.js
@@ -1,7 +1,13 @@
+// Load helpers.
 import CSVtoJSON from './CSVtoJSON.js';
 
 export const getData = () => fetch((window.location.href.includes('gsfo.org')) ? 'https://storage.unctad.org/2022-sustainable_fund/assets/data/sustainability_fund - data.csv' : './assets/data/sustainability_fund - data.csv')
-  .then(response => response.text())
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response.text();
+  })
   .then(body => CSVtoJSON(body));
 
 export default getData;
